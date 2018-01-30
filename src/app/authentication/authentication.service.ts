d5c93c708be58e458d9d1e3952e34ea6f7143e02
David Leblanc <David.LeBlanc@hamiltoncompany.com>
2018-01-30 07:53:30 -0800

    login now works

--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -5,16 +5,38 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
 import { Observable } from 'rxjs/Observable';
 
+import { SkypeClient } from '../skype-client';
+
 @Injectable()
 export class AuthenticationService implements OnInit {
-    constructor() { }
+    constructor(private skypeClient: SkypeClient) { }
 
     ngOnInit() {
 
     }
 
-    login(username: string, password: string): Observable<any> {
-      return null;
+    login(username: string, password: string): Observable<boolean> {
+      return new Observable<boolean>(observer => {
+        if(this.skypeClient.clientInstance.signInManager.state() == "SignedIn") {
+          observer.next(true);
+          observer.complete();
+        }
+        else {
+          this.skypeClient.clientInstance.signInManager.signIn({
+            username: username,
+            password: password,
+            domain: '.skype'
+          }).then(() => {
+            observer.next(true);
+            observer.complete();
+          },
+          error => {
+            console.error(error);
+            observer.next(false);
+            observer.complete();
+          })
+        }
+      })
     }
 
     logout(): any {
